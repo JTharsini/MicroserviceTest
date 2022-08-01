dc69ca2f7339ab21dc2e8abf72576f14f24842ba
Jeyatharsini <6tharshi@gmail.com>
2022-08-02 00:54:25 +0530

    added timeout to movie catalog service

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -16,7 +17,10 @@ public class MovieCatalogServiceApplication
   @LoadBalanced
   public RestTemplate getRestTemplate()
   {
-    return new RestTemplate();
+    //return new RestTemplate();
+    HttpComponentsClientHttpRequestFactory httpComponentsClientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+    httpComponentsClientHttpRequestFactory.setConnectTimeout(3000);// in milliseconds
+    return new RestTemplate(httpComponentsClientHttpRequestFactory);
   }
 
   @Bean
