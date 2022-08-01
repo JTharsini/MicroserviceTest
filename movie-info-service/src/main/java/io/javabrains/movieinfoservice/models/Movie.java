6b3b85673c763c382b7fa89451cd2f22f46a7009
Jeyatharsini <6tharshi@gmail.com>
2022-08-01 23:52:57 +0530

    consume external service from movie-info-service

--- movie-info-service/src/main/java/io/javabrains/movieinfoservice/models/Movie.java
+++ movie-info-service/src/main/java/io/javabrains/movieinfoservice/models/Movie.java
@@ -4,11 +4,13 @@ public class Movie
 {
   private String movieId;
   private String name;
+  private String overview;
 
-  public Movie(String movieId, String name)
+  public Movie(String movieId, String name, String overview)
   {
     this.movieId = movieId;
     this.name = name;
+    this.overview = overview;
   }
 
   public String getMovieId()
@@ -29,5 +31,13 @@ public class Movie
   public void setName(String name)
   {
     this.name = name;
+  }
+
+  public String getOverview() {
+    return overview;
+  }
+
+  public void setOverview(String overview) {
+    this.overview = overview;
   }
 }
